88c2a41c867e571726485d4485e51ca52fbbd797
SebastianGonnsen <SebastianGonnsen@users.noreply.github.com>
2024-06-06 18:48:35 +0000

    Fix styling

--- src/ExcludeWebhusetServiceProvider.php
+++ src/ExcludeWebhusetServiceProvider.php
@@ -2,20 +2,20 @@
 
 namespace SebastianGonnsen\ExcludeWebhuset;
 
-use Illuminate\Support\ServiceProvider;
 use App\Models\User;
+use Illuminate\Support\ServiceProvider;
 
 class ExcludeWebhusetServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/excludewebhuset.php', 'excludewebhuset');
+        $this->mergeConfigFrom(__DIR__.'/../config/excludewebhuset.php', 'excludewebhuset');
     }
 
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../config/excludewebhuset.php' => config_path('excludewebhuset.php'),
+            __DIR__.'/../config/excludewebhuset.php' => config_path('excludewebhuset.php'),
         ]);
 
         $excludedDomain = config('excludewebhuset.excluded_domain', '%@webhusetballum.dk');
